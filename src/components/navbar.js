e1512c80a7be25554686a9ef15dfb24b3f9b9845
Saket Zode <55745771+SaketZode@users.noreply.github.com>
2020-11-01 22:26:05 +0530

    separated navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react"
+
+
+class NavBar extends Component {
+    render() {
+        return(
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="/">Expenses Manager</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/register">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/login">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/aboutus">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/contactus">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+
+export default NavBar
